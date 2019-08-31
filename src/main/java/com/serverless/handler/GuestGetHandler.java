3d83133c5814c51e035e23698332405a4c288e1d
Julian Ignacio Lopez Scala <julianignacio.lopez@globant.com>
2019-08-31 10:27:07 -0500

    JLS uploaded first classes for Post guest

--- src/main/java/com/serverless/handler/GuestGetHandler.java
+++ src/main/java/com/serverless/handler/GuestGetHandler.java
@@ -3,38 +3,20 @@ package com.serverless.handler;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.serverless.function.GuestPostConsumer;
-import com.serverless.model.Guest;
-import com.serverless.util.GuestMapper;
+import com.serverless.ApiGatewayResponse;
+import org.apache.log4j.Logger;
 
-import java.io.IOException;
+public class GuestGetHandler implements RequestHandler<APIGatewayProxyRequestEvent, ApiGatewayResponse> {
 
-public class GuestGetHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-    @Override
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
-        APIGatewayProxyResponseEvent apiGatewayProxyResponseEvent = new APIGatewayProxyResponseEvent();
-        if (process(input)) {
-            apiGatewayProxyResponseEvent.setStatusCode(200);
-        } else {
-            apiGatewayProxyResponseEvent.setStatusCode(404);
-            apiGatewayProxyResponseEvent.setBody("Guest add failure");
-        }
-        return apiGatewayProxyResponseEvent;
-    }
-
-    private boolean process(APIGatewayProxyRequestEvent input) {
-        GuestPostConsumer guestPostConsumer = new GuestPostConsumer();
-        try {
-            Guest guest = GuestMapper.getMapper().readValue(input.getBody(), Guest.class);
-            guestPostConsumer.accept(guest);
-
-            return true;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return true;
-    }
+	@Override
+	public ApiGatewayResponse handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+		//LOG.info("received: " + input.);
+		//Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
+		return /*ApiGatewayResponse.builder()
+				.setStatusCode(200)
+				.setObjectBody(responseBody)
+				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
+				.build()*/null;
+	}
 }
